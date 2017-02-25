8feef89e13608da36ac8fbdd24dc19d8ff089974
Justin <justintenser@me.com>
2017-02-25 16:42:56 -0500

    nested routes within nested routes. created users/1/jobs/1/review/1

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  resources :users, only: [:new, :create, :show]
-  resources :jobs do
-    resources :reviews, only: [:show, :create, :destroy]
+  resources :users do
+    resources :jobs do
+      resources :reviews, only: [:show, :new, :destroy]
+    end
   end
 end

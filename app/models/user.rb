2a1452e384c2244105c4d1e25c605564187f811d
noah tenser <ntenser@hotmail.com>
2017-02-25 16:30:32 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :password, confirmation: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+
+  validates :email, uniqueness: true
 end

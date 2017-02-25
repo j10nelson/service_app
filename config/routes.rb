7053ba551b05435eff7911fea2809ddd430f8ac5
noah tenser <ntenser@hotmail.com>
2017-02-25 15:49:36 -0500

    add a route for users, sessions and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw
+  root "jobs#index"
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end

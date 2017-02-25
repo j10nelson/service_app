7c324f7d63a7514e2339a7367ae8016d890f4980
Justin <justintenser@me.com>
2017-02-25 17:00:34 -0500

    filled out the jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,29 +1,43 @@
 class JobsController < ApplicationController
 
   def index
-
+    @jobs = Job.all
   end
 
   def show
-
+    @job = Job.find(params:[id])
   end
 
   def new
+    @job = Job.new
   end
 
   def edit
-
+    @job = Job.find(params:[id])
   end
 
   def create
+    @job = Job.new(job_params)
 
+    if @job.save
+      redirect_to current_user_url
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    @job = Job.find(params:[id])
+      @job.destroy
+      redirect_to current_user_url
+    end
   end
 
-
+  private
+  
+  def job_params
+    param.require(:job).permit(:details, :date, :time)
+  end
 
 
 end

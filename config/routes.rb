dec34bd9d0a6e9cf0bebfae0dfa3c56241e3f2f9
Justin <justintenser@me.com>
2017-02-25 16:13:39 -0500

    create user controler and create destroy edit show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :jobs do

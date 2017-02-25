05c9725d7b7eaef0b1087c1eaa4d96e364d0475d
noah tenser <ntenser@hotmail.com>
2017-02-25 16:26:35 -0500

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw
+Rails.application.routes.draw do
   root "jobs#index"
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]

a79b183237347549b0f1b7803abfbc7e760328dd
noah tenser <ntenser@hotmail.com>
2017-02-25 16:03:40 -0500

    set up the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       flash[:notice] = "Signed up!"
+      auto_login(@user)
+      redirect_to projects_url
+    else
+      flash.now[:error] = 'Sorry, try again!'
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number,:home_address, :billing_address, :company_name, :trade, :license_plate, :email, :password, :password_confirmation)
+  end
 end

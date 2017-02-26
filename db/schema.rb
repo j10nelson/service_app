5b54b657c03de02b3585a04df8aaa1667f49fd51
Justin <justintenser@me.com>
2017-02-26 17:25:56 -0500

    done for the night sunday

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226215718) do
+ActiveRecord::Schema.define(version: 20170226220753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20170226215718) do
     t.integer  "service_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "date"
-    t.datetime "time"
+    t.date     "date"
+    t.integer  "time"
   end
 
   create_table "reviews", force: :cascade do |t|

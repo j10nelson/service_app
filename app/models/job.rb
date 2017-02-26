14edf911cd6c07af011f77215edf560769072473
noah tenser <ntenser@hotmail.com>
2017-02-26 18:43:31 -0500

    add associations to models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  belongs_to :service
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :jobs
+  belongs_to :trade
+  has_many :reviews
+  has_many :reviews, through :jobs
+
 validates :password, length: { minimum: 4 }, on: :create
 validates :password, confirmation: true, on: :create
 validates :password_confirmation, presence: true, on: :create

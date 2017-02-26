f2c1c05c6282f66ac2a9edb247c7adddb0867a16
Justin <justintenser@me.com>
2017-02-26 12:45:06 -0500

    migrate and created user table using sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226173454) do
+ActiveRecord::Schema.define(version: 20170226174232) do
 
   create_table "jobs", force: :cascade do |t|
     t.integer  "user_id"
@@ -31,6 +31,30 @@ ActiveRecord::Schema.define(version: 20170226173454) do
     t.string   "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone_number"
+    t.string   "home_address"
+    t.string   "billing_address"
+    t.string   "company_name"
+    t.string   "trade"
+    t.string   "license_plate"
+    t.string   "email",                           null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end

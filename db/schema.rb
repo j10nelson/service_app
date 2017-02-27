2dceb2cf2cd33a4a330d8d1b16bfc5eca8fa2f2a
noah tenser <ntenser@hotmail.com>
2017-02-26 20:04:53 -0500

    add title column to job table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226234515) do
+ActiveRecord::Schema.define(version: 20170227010335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170226234515) do
     t.datetime "updated_at", null: false
     t.date     "date"
     t.time     "time"
+    t.string   "title"
   end
 
   create_table "reviews", force: :cascade do |t|

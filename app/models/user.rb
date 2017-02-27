fa94ae7256ad425736c541d4193d7bf0366dc88b
noah tenser <ntenser@hotmail.com>
2017-02-26 19:45:23 -0500

    fix user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   has_many :jobs
   belongs_to :trade
   has_many :reviews
-  has_many :reviews, through :jobs
+  has_many :reviews, through: :jobs
 
-validates :password, length: { minimum: 4 }, on: :create
-validates :password, confirmation: true, on: :create
-validates :password_confirmation, presence: true, on: :create
+  validates :password, length: { minimum: 4 }, on: :create
+  validates :password, confirmation: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
 
-validates :email, uniqueness: true
+  validates :email, uniqueness: true
 end

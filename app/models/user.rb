2b0675130253de3e03bd7aaf8b4b941dd5355798
noah tenser <ntenser@hotmail.com>
2017-02-26 20:23:13 -0500

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :reviews, through: :jobs
 
+  validates :first_name, :last_name, :email, presence: true, on: :create
   validates :password, length: { minimum: 4 }, presence: true, on: :create
   validates :password, confirmation: true, on: :create
   validates :password_confirmation, presence: true, on: :create

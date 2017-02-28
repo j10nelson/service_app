df81ae6e20cfe470f87178599ea1b0bbe2cb7564
Justin <justintenser@me.com>
2017-02-28 16:28:16 -0500

    removed trades table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228010145) do
+ActiveRecord::Schema.define(version: 20170228212431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,12 +48,6 @@ ActiveRecord::Schema.define(version: 20170228010145) do
   create_table "services_users", force: :cascade do |t|
     t.integer "services_id"
     t.integer "worker_id"
-  end
-
-  create_table "trades", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

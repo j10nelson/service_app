4b06a67cdb1d55f4db8ef37979bc5ebb1fa1a170
Justin <justintenser@me.com>
2017-03-03 16:19:56 -0500

    added review to job table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302181015) do
+ActiveRecord::Schema.define(version: 20170303211734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170302181015) do
     t.time     "time"
     t.string   "title"
     t.string   "notes"
+    t.string   "review"
   end
 
   create_table "reviews", force: :cascade do |t|

326616d93029154dc1620fe939bfe5c1c15cb4eb
Justin <justintenser@me.com>
2017-03-03 18:29:40 -0500

    created form/new page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,23 @@
 class ReviewsController < ApplicationController
   before_action :require_login
-  
-  def index
-  end
+
+
 
   def show
 
   end
 
   def new
+    @job = Job.find(params[:job_id])
+    @review = Review.new
+
+  end
+
+  def create
+
 
   end
 
 
-  def edit
-
-  end
-
-  def destroy
-
-  end
 
 end
--- db/migrate/20170303211734_add_review_to_jobs.rb
+++
@@ -1,7 +0,0 @@
-class AddReviewToJobs < ActiveRecord::Migration[5.0]
-  def change
-    change_table :jobs do |t|
-      t.string :review_id
-    end
-  end
-end

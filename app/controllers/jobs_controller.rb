3f1525c0d25bc8868500dc4e3d667a572bf122e7
Justin <justintenser@me.com>
2017-03-02 22:35:43 -0500

    added method patch, didnt do much else

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -36,8 +36,6 @@ class JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
-
-
     if @job.update_attributes(job_params)
       # redirect_to "/jobs/#{@job.id}"
       redirect_to current_user

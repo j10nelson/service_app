82b84a3a9c8344ad11ead4d1fc435814a78b07ca
Justin <justintenser@me.com>
2017-03-04 15:02:16 -0500

    fixed seeds - added price

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,27 +12,32 @@ Service.destroy_all
 Service.create!(
 trade: "hairdresser",
 type_of_service: "Cut",
+price: 20
 )
 
 Service.create!(
 trade: "massage therapist",
 type_of_service: "Deep Tissue",
+price: 80
 )
 
 Service.create!(
-trade: "nail",
+trade: "manicure",
 type_of_service: "Paint Nails",
+price: 40
 )
 
 
 Service.create!(
 trade: "pool cleaning",
 type_of_service: "Clean Pool",
+price: 100
 )
 
 Service.create!(
 trade: "snow removal",
 type_of_service: "Clean Driveway",
+price: 90
 )
 
 

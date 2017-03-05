c547ac2152b6156172221f4254ee8ede51912f0d
noah tenser <ntenser@hotmail.com>
2017-03-04 23:45:07 -0500

    fix format and spacing in user show page and remove methods from review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,20 +2,5 @@ class Review < ApplicationRecord
   belongs_to :job
   belongs_to :user
 
-  def submitted?
-    if self.user_id
-      return true
-    else
-      return false
-    end
-  end
-
-  def not_submitted?
-    if self.user_id
-      return false
-    else
-      return true
-    end
-  end
 
 end

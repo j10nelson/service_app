f2422eb26d1e33a4a84cceace242acedeedcdba9
Justin <justintenser@me.com>
2017-03-06 17:15:42 -0500

    droppped trades table

---
+++ db/migrate/20170306221447_delete_trades.rb
@@ -0,0 +1,7 @@
+class DeleteTrades < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :trades
+
+    
+  end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306221054) do
+ActiveRecord::Schema.define(version: 20170306221447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +48,6 @@ ActiveRecord::Schema.define(version: 20170306221054) do
   create_table "services_users", force: :cascade do |t|
     t.integer "worker_id"
     t.integer "services_id"
-  end
-
-  create_table "trades", force: :cascade do |t|
-    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|

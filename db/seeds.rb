3794fb2f0111ea7abbe6a9d813f75bce4ea34b80
Justin <justintenser@me.com>
2017-03-06 21:36:35 -0500

    post merge fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,32 +10,27 @@ puts "beginning seed file"
 Service.destroy_all
 
 Service.create!(
-trade: "hairdresser",
 type_of_service: "Cut",
 price: 20
 )
 
 Service.create!(
-trade: "massage therapist",
 type_of_service: "Deep Tissue",
 price: 80
 )
 
 Service.create!(
-trade: "manicure",
 type_of_service: "Paint Nails",
 price: 40
 )
 
 
 Service.create!(
-trade: "pool cleaning",
 type_of_service: "Clean Pool",
 price: 100
 )
 
 Service.create!(
-trade: "snow removal",
 type_of_service: "Clean Driveway",
 price: 90
 )

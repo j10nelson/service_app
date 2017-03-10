14b8bf2800953c5044820f45b7eefae8aef7be14
Justin <justintenser@me.com>
2017-03-10 13:09:10 -0500

    removed console

--- db/migrate/20170227203607_remove_column_from_trades_table.rb
+++ db/migrate/20170227203607_remove_column_from_trades_table.rb
@@ -1,8 +1,8 @@
-# class RemoveColumnFromTradesTable < ActiveRecord::Migration[5.0]
-#   def change
-#     change_table :trades do |t|
-#       t.remove :worker_id
-#       t.remove :user_id
-#     end
-#   end
-# end
+class RemoveColumnFromTradesTable < ActiveRecord::Migration[5.0]
+  def change
+    change_table :trades do |t|
+      t.remove :worker_id
+      t.remove :user_id
+    end
+  end
+end

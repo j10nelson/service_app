a9832b0503b80dba3715ab87847957ac9b52f2b8
Justin <justintenser@me.com>
2017-03-14 16:18:05 -0400

    added category to trades

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313042311) do
+ActiveRecord::Schema.define(version: 20170314200957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170313042311) do
 
   create_table "trades", force: :cascade do |t|
     t.string "name"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|

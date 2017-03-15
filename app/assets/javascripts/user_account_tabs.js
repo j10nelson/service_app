6a83c3233fb2ec49d51f48bcc7ed4e8dffe7773a
Justin <justintenser@me.com>
2017-03-15 15:09:14 -0400

    fixed JS clicking issue

--- app/assets/javascripts/user_account_tabs.js
+++ app/assets/javascripts/user_account_tabs.js
@@ -1,5 +1,5 @@
 document.addEventListener("turbolinks:load", function(){
-  $('.side-menu').on('click', function(event) {
+  $('.side-menu li').on('click', function(event) {
     event.preventDefault()
     var jobsDashboard = event.target.dataset.tab;
     $('.jobs-dashboard').removeClass('show').removeClass('wtv');

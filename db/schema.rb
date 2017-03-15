2a8f5d364426f0689a104847bdcc3e440dae19f8
Justin <justintenser@me.com>
2017-03-15 13:17:37 -0400

    after merge justin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313205315) do
+ActiveRecord::Schema.define(version: 20170315050104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 20170313205315) do
     t.integer  "service_id"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
-    t.date     "date"
-    t.time     "time"
     t.string   "title"
     t.text     "note"
     t.string   "state",         default: "pending"

24b66bc030544f32f4a9212c0630a77dd51897fd
noah tenser <ntenser@hotmail.com>
2017-03-18 17:13:55 -0400

    if address validations occur user is redirected back to the previous page - validation messages do not show up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,22 @@ class User < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   has_many :jobs, -> { order('updated_at DESC').uniq }
-
   # has_one :worker :source :user
   has_many :services, through: :trade
   has_many :reviews
   has_many :reviews, through: :jobs
   belongs_to :trade
   has_one :address
-
   has_one :submission
-
 
   validates :first_name, :last_name, :email, presence: true, on: :create
   validates :password, length: { minimum: 4 }, presence: true, on: :create
   validates :password, confirmation: true, on: :create
   validates :password_confirmation, presence: true, on: :create
   validates :email, uniqueness: true
+  # validates :phone_number, length: { maximum: 9 }, presence: true
 
   # ROLES = ["client", "worker", "admin"]
-
   # attr_accessor :house_number, :apt_number, :street, :province, :country, :postal_code, :city
 
   def address?

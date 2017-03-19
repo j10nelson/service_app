a185bff835d901211f438038ffd7ac587c373867
Justin <justintenser@me.com>
2017-03-19 17:03:26 -0400

    fixed styles, fixed flash, fixed menu, fixed admin account, fixed redirect to home when logged in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,18 +5,19 @@ class UserSessionsController < ApplicationController
 
     def create
        if @user = login(params[:email], params[:password], params[:remember_me])
-
         if params[:origin] = "email_link"
-          redirect_to user_url(current_user.id)
+          redirect_to root_url, notice: 'Login successful'
+          # redirect_to user_url(current_user.id), notice: 'Login successful'
 
-        else
-          redirect_back_or_to(root_url)
-        end
-        #  notice: 'Login successful'
+        # else
+        #   redirect_back_or_to(root_url)
+        # end
+        #   # redirect_to current_user
       else
         flash.now[:alert] = 'Login failed'
         render action: 'new'
       end
+    end
 
 
     end

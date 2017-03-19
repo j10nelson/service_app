ead220ccfb0299e5ccacca879922f452a346575a
noah tenser <ntenser@hotmail.com>
2017-03-19 17:03:55 -0400

    fix format user controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,29 +1,25 @@
 class UserSessionsController < ApplicationController
-    def new
-      @user = User.new
-    end
-
-    def create
-       if @user = login(params[:email], params[:password], params[:remember_me])
-
-        if params[:origin] = "email_link"
-          redirect_to user_url(current_user.id)
-
-        else
-          redirect_back_or_to(root_url)
-        end
-        #  notice: 'Login successful'
-      else
-        flash.now[:alert] = 'Login failed'
-        render action: 'new'
-      end
-
-
-    end
-
-    def destroy
-      logout
-      redirect_to root_url, notice: 'Logged out!'
+  def new
+    @user = User.new
   end
 
+  def create
+    if @user = login(params[:email], params[:password], params[:remember_me])
+
+      if params[:origin] = "email_link"
+        redirect_to user_url(current_user.id)
+      else
+        redirect_back_or_to(root_url)
+      end
+        #  notice: 'Login successful'
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_url, notice: 'Logged out!'
+  end
 end

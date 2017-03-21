7146c2223ff55216e4475488bcbc772435da6e7e
Justin <justintenser@me.com>
2017-03-20 20:18:22 -0400

    trying to get photo to save and display

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -9,14 +9,17 @@ class SubmissionsController < ApplicationController
     @submission = Submission.new(worker_request)
     @submission.request_state = "submitted"
     @submission.user_id = current_user.id
+    # @user.photo = params[:photo][:photo]
 
     if @submission.save
+      # @user.save
+
       redirect_to current_user
     end
   end
 
 private
   def worker_request
-    params.require(:submission).permit(:trade_requested, :deliverables_requested, :about_requested)
+    params.require(:submission).permit(:trade_requested, :deliverables_requested, :about_requested, :photo)
   end
 end
